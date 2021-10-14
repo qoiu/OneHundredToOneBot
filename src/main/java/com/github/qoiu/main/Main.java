549a91ce73425110a44bdc6ea0d535429c369a35
Nikita <guschinnick@gmail.com>
2021-10-14 05:21:13 +0300

    init commit

---
+++ src/main/java/com/github/qoiu/main/Main.java
@@ -0,0 +1,17 @@
+package main.java.com.github.qoiu.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("100 to 1");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
